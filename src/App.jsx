3cd8c979ebe968823c5085d1fd01e7fe6639567c
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-15 17:06:43 +0300

    Update project files

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="__className_92e931 __variable_00e045 __variable_315a98 antialiased font-medium dark">
+      <div>Section 1: No screenshot</div>
+    </div>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-1': '#3f3f3f',
+        'custom-2': '#e8e8e8',
+        'custom-3': '#fff',
+        'custom-4': '#000',
+        'custom-5': '#ff96f9',
+        'custom-6': '#d97757',
+        'custom-7': '#000000',
+        'custom-8': '#ffffff',
+        'custom-9': '#ffa500'
+      },
+      fontFamily: {
+        'custom-1': ['ui-sans-serif', 'sans-serif'],
+        'custom-2': ['system-ui', 'sans-serif'],
+        'custom-3': ['-apple-system', 'sans-serif']
+      }
+    }
+  }
+};
